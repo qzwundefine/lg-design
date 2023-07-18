7cbd7bfa613220a7f15fe5ebe78ca00ec7a68780
zhiweiqiu <8169856+zhiweiqiu@user.noreply.gitee.com>
2023-07-18 17:54:12 +0800

    feat config

--- .dumirc.ts
+++ .dumirc.ts
@@ -5,6 +5,8 @@ export default defineConfig({
   themeConfig: {
     name: 'Lg Design',
   },
+  base: '/lg-design',
+  publicPath: '/lg-design/',
   plugins: [require.resolve('@umijs/plugins/dist/unocss')],
   unocss: {
     // 检测 className 的文件范围，若项目不包含 src 目录，可使用 `pages/**/*.tsx`
